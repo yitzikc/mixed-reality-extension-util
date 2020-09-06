26ef9eccdca650b9448a40f1f187b1438c88a4ec
Yitzik Casapu <yitzikc@gmail.com>
2020-09-06 03:37:04 +0100

    Simplify the implementation of getParameterLastValue()

--- src/parameter-set-util.ts
+++ src/parameter-set-util.ts
@@ -1,15 +1,9 @@
+import { castArray, last } from 'lodash';
 import { ParameterSet, Color3, Color4 } from '@microsoft/mixed-reality-extension-sdk';
 
 // Get the last of potentially multiple values of a parameter in an MRE parameter set
 export function getParameterLastValue(params: ParameterSet, name: string, dflValue: string = ''): string {
-  const value = params[name];
-  if (typeof value === 'string') {
-    return value;
-  } else if (Array.isArray(value)) {
-    return value[value.length - 1];
-  }
-
-  return dflValue;
+  return <string> last(castArray(params[name] ?? dflValue));
 }
 
 // Get the value of a boolean parameter whose value can be 'y' or 'n'
