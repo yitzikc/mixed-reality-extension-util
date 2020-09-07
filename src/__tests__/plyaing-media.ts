fbab75f1fbe6f09e06199536e9450585f337d813
Yitzik Casapu <yitzikc@gmail.com>
2020-09-08 00:26:18 +0100

    Unit test for volume changes

---
+++ src/__tests__/plyaing-media.ts
@@ -0,0 +1,21 @@
+import {
+    PlayingMedia
+} from '..';
+
+
+test('test volume-up-down', () => {
+    const pm = new PlayingMedia(undefined, { volume: 0.5});
+    expect(pm.currentVolume).toEqual(0.5);
+    pm.changeVolume(25);
+    expect(pm.currentVolume).toEqual(0.625);
+    pm.changeVolume(-20);
+    expect(pm.currentVolume).toEqual(0.5);
+    pm.changeVolume(50);
+    expect(pm.currentVolume).toEqual(0.75);
+    pm.changeVolume(50);
+    expect(pm.currentVolume).toEqual(1);
+    pm.setState({volume: 0.5});
+    expect(pm.currentVolume).toEqual(0.5);
+    pm.changeVolume(-100);
+    expect(pm.currentVolume).toEqual(0);
+});
