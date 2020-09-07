3a6c798fc7badffb617dd2608a3651f399ee1941
Yitzik Casapu <yitzikc@gmail.com>
2020-09-08 00:34:24 +0100

    Check the behavior after the volume has been clipped

--- src/__tests__/playing-media.ts
+++ src/__tests__/playing-media.ts
@@ -14,6 +14,10 @@ test('test volume-up-down', () => {
     expect(pm.currentVolume).toEqual(0.75);
     pm.changeVolume(50);
     expect(pm.currentVolume).toEqual(1);
+    pm.changeVolume(10);
+    expect(pm.currentVolume).toEqual(1);
+    pm.changeVolume(-20);
+    expect(pm.currentVolume).toEqual(0.8);
     pm.setState({volume: 0.5});
     expect(pm.currentVolume).toEqual(0.5);
     pm.changeVolume(-100);
